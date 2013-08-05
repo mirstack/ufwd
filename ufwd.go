de1a4d7cfe9cc97d234ef37d6bf00da7cff8fe9d
Chris Kowalik <chris@nu7hat.ch>
2013-08-04 22:10:35 -0300

    simple tested implementation of forwarding udp server

---
+++ ufwd.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"net"
+	"log"
+	"io"
+	"fmt"
+	"flag"
+)
+
+type Config struct {
+	BufSize  uint
+	Proto    string
+	Addr     string
+	DestAddr string
+	Debug    bool
+}
+
+var conf = new(Config)
+
+func init() {
+	log.SetFlags(0)
+
+	flag.UintVar(&conf.BufSize, "bufsize", 1024, "read/write buffer maximum size")
+	flag.StringVar(&conf.Proto, "proto", "udp4", "which protocol to use (udp4 or udp6)")
+	flag.StringVar(&conf.Addr, "addr", "", "bind address")
+	flag.StringVar(&conf.DestAddr, "dest", "", "destination address")
+	flag.BoolVar(&conf.Debug, "debug", false, "enable debug mode")
+	flag.Parse()
+}
+
+func main() {
+	if conf.BufSize == 0 {
+		log.Fatalf("ERROR: bufsize must be greater than 0")
+	}
+
+	conn, err := dstConn(conf)
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+
+	defer conn.Close()
+	log.Fatalf("ERROR: %v", srvLoop(conf, conn))
+}
+
+func debugf(format string, args ...interface{}) {
+	if conf.Debug {
+		log.Printf("DEBUG: " + format, args...)
+	}
+}
+
+func dstConn(c *Config) (io.WriteCloser, error) {
+	addr, err := net.ResolveUDPAddr(c.Proto, c.DestAddr)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := net.DialUDP(c.Proto, nil, addr)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func srvLoop(c *Config, out io.Writer) error {
+	addr, err := net.ResolveUDPAddr(c.Proto, c.Addr)
+	if err != nil {
+		return err
+	}
+	sock, err := net.ListenUDP(c.Proto, addr)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		log.Printf("INFO: shutting down the server")
+		sock.Close()
+	}()
+
+	log.Printf("INFO: about to start bridge from %s to %s", c.Addr, c.DestAddr)
+	var buf []byte = make([]byte, c.BufSize)
+
+	for {
+		n, err := sock.Read(buf[0:])
+		if err != nil {
+			return err
+		}
+		debugf("received packet of size %d", n)
+		debugf("> %s", buf[:n])
+
+		w, err := out.Write(buf[:n])
+		if err != nil {
+			return err
+		}
+		debugf("passed packet of size %d", w)
+
+		if n != w {
+			err = fmt.Errorf("corrupted packet, written %d out of %d bytes", w, n)
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ ufwd_test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"testing"
+	"net"
+	"time"
+	"fmt"
+	"log"
+	"io/ioutil"
+)
+
+var msgs = make(chan string)
+
+func init() {
+	log.SetOutput(ioutil.Discard)
+
+	conf = &Config{
+		BufSize:  1024,
+		Proto:    "udp4",
+		Addr:     ":5566",
+		DestAddr: ":5567",
+		Debug:    true,
+	}
+
+	ready := make(chan bool, 1)
+
+	go func() {
+		addr, err := net.ResolveUDPAddr(conf.Proto, conf.DestAddr)
+		if err != nil {
+			panic(err)
+		}
+		sock, err := net.ListenUDP(conf.Proto, addr)
+		if err != nil {
+			panic(err)
+		}
+		var buf []byte = make([]byte, conf.BufSize)
+		ready <- true
+		for {
+			n, err := sock.Read(buf[0:])
+			if err != nil {
+				fmt.Printf("%v", err)
+				continue
+			}
+			msgs <- string(buf[:n])
+		}
+	}()
+
+	<-ready
+
+	go main()
+	<-time.After(100 * time.Millisecond)
+}
+
+func TestServer(t *testing.T) {
+	addr, err := net.ResolveUDPAddr(conf.Proto, conf.DestAddr)
+	if err != nil {
+		t.Errorf("Expected to resolve the addr, got error: %v", err)
+		return
+	}
+	conn, err := net.DialUDP(conf.Proto, nil, addr)
+	if err != nil {
+		t.Errorf("Expected to connect with the server, got error: %v", err)
+		return
+	}
+
+	messages := []string{
+		"hello world!",
+		"another message to make sure it works",
+		"and one more time",
+	}
+
+	for _, msg := range messages {
+		_, err = conn.Write([]byte(msg))
+		if err != nil {
+			t.Errorf("Expected to write data, got error: %v", err)
+			continue
+		}
+
+		select {
+		case rmsg := <- msgs:
+			if rmsg != msg {
+				t.Errorf("Expected to get '%s', got '%s'", msg, rmsg)
+			}
+		case <-time.After(1 * time.Second):
+			t.Errorf("Expected to get a message, got nothing")
+		}
+	}
+}
